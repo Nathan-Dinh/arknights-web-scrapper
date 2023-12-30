43c56a3738957f82a8509e94b9c20dee8738283c
nathandinh <nathan.dinh03@outlook.com>
2023-12-29 23:32:44 -0500

    Implemented a new skin collection

--- Operator-Information.py
+++ Operator-Information.py
@@ -1,52 +1,75 @@
 # Author: Nathan Dinh
-# Revision: December 20 2023 - Nathan Dinh 
+# Revision: December 29 2023 - Nathan Dinh 
 
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
-DOMAIN_NAME_URL = str("https://gamepress.gg")
-SUB_FOLDER_URL = str("/arknights/tools/interactive-operator-list#tags=null##stats")
-
-# Mongodb connection string goes here example: mongodb+srv://[username:password@]host[/[defaultauthdb][?options]]
-MONGODB_CONNECTION_STRING = str("<Connection string goes here>")
-
-# Database and collection name
-DATABASE_NAME = str("<mongodb database name goes here>")
-COLLECTION_NAME = str("<collection name goes here>")
-
+# Imports all variables from .env file 
+config = {**dotenv_values("./config/.env.secret")}
 # Connect your application to your database
-clu = MongoClient(MONGODB_CONNECTION_STRING)
-db = clu[DATABASE_NAME]
-collection = db[COLLECTION_NAME]
+client = MongoClient(config['MONGODB_CONNECTION_STRING'])
+database = client[config['DATABASE_NAME']]
+operatorCollection = database[config['OPERATORS_COLLECTION']]
+skinCollection = database[config['SKINS_COLLECTION']]
 
-operatorId = 0;
+# Collection constraints
+operatorCollection.create_index('name', unique=True)
+skinCollection.create_index('skinName', unique=True)
 
 # Operate class
-class OperatorInformation:
-    def __init__(self,id,imageUrl,name,operatorTags,operatorArc,operatorProfile):
-        self.id = int(id)
+class OperatorInformationTemplate:
+    def __init__(self,imageUrl,name,operatorTags,operatorArc,operatorProfile):
         self.imageUrl = str(imageUrl)
         self.name = str(name)
         self.operatorTags = operatorTags
         self.operatorArc = str(operatorArc)
         self.operatorProfile = str(operatorProfile)
 
+class SkinInformationTemplate:
+    def __init__(self,imageUrl,skinName,operatorName):
+        self.imageUrl = str(imageUrl)
+        self.skinName = str(skinName)
+        self.operatorName = str(operatorName)
+
 # Method will save the operators information to your database
-def save_operator(operator):
+def save_operator(operatorTemplate):
     try:
-        postBody = {"_id" : operator.id, 
-                  "imgUrl":operator.imageUrl, 
-                  "name": operator.name,
-                  "recruitmentTag": operator.operatorTags,
-                  "archetype": operator.operatorArc,
-                  "profile": operator.operatorProfile,}
-        collection.insert_one(postBody)
-        print(operator.name + ":Saved")
+        postBody = {"imgUrl":operatorTemplate.imageUrl, 
+                  "name": operatorTemplate.name,
+                  "recruitmentTag": operatorTemplate.operatorTags,
+                  "archetype": operatorTemplate.operatorArc,
+                  "profile": operatorTemplate.operatorProfile,}
+        operatorCollection.insert_one(postBody)
+        print(operatorTemplate.name + ":Saved")
     except Exception as e:
         print(str(e))
-
     return
+
+def save_skin(skinTemplate):
+    try:
+        postBody = {"imgUrl":skinTemplate.imageUrl, 
+                  "skinName": skinTemplate.skinName,
+                  "operatorName": skinTemplate.operatorName}
+        skinCollection.insert_one(postBody)
+        print(skinTemplate.skinName + ":Saved")
+    except Exception as e:
+        print(str(e))
+    return
+
+def get_skin_information(htmlContext):
+    try:
+        operatorName = htmlContext.find(class_="views-field views-field-field-skin-operator").a.get_text().lower()
+        skinLink = htmlContext.a['href']
+        req = requests.get(config['DOMAIN_NAME']+skinLink)
+        ctx = BeautifulSoup(req.content, "html.parser")
+        imageLink = ctx.find(id="image-tab-1").a['href']
+        skinName = ctx.find(id="page-title").h1.get_text().lower()
+        return SkinInformationTemplate(imageLink,skinName,operatorName)
+    except Exception as e:
+           print(e)
+           raise ValueError(str(operatorName))
 
 def get_operator_information(htmlContext):
         try:
@@ -62,32 +85,41 @@ def get_operator_information(htmlContext):
                 operatorArc = operatorTags[t]
                 operatorTags.pop(len(operatorTags) - 1)
 
-            op = OperatorInformation(operatorId,operatorImage,operatorName,operatorTags, operatorArc,operatorProfile[0].get_text())
+            return  OperatorInformationTemplate(operatorImage,operatorName,operatorTags, operatorArc,operatorProfile[0].get_text())
         except Exception as e:
            print(e)
            raise ValueError(str(operatorName))
-        return op
 
+reqOperatorListPage = requests.get(config['DOMAIN_NAME'] + config['OPERATOR_LIST_URL'])
+ctxOperatorListHtml = BeautifulSoup(reqOperatorListPage.content, "html.parser")
+operatorUrls = ctxOperatorListHtml.find_all(class_='operator-title-actual') # Collects the operators url 
 
-req = requests.get(DOMAIN_NAME_URL + SUB_FOLDER_URL)
-ctx = BeautifulSoup(req.content, "html.parser")
-
-# Collects the operators url 
-operatorLink = ctx.find_all(class_='operator-title-actual')
-
-for o in operatorLink :
-    req = requests.get(DOMAIN_NAME_URL + o['href'])
-    ctx = BeautifulSoup(req.content, "html.parser")
-    operatorId += 1
-    
+for link in operatorUrls :
+    req = requests.get(config['DOMAIN_NAME'] + link['href'])
+    operatorProfilePage = BeautifulSoup(req.content, "html.parser")
     try:
-        operatorInformation = get_operator_information(ctx)
+        operatorInformation = get_operator_information(operatorProfilePage)
         save_operator(operatorInformation)
     except ValueError as e:
         print(str(e) + ": Could not be saved")
+    break
         
+reqSkinListPage = requests.get(config['DOMAIN_NAME'] + config['SKIN_LIST_URL'])
+ctxSkinListHtml = BeautifulSoup(reqSkinListPage.content, "html.parser")
+ctxSkinListHtml = ctxSkinListHtml.find(id="topic-511941")
+ctxSkinListHtml = ctxSkinListHtml.find('tbody')
+ctxTableRows = ctxSkinListHtml.find_all('tr')
 
-    
+for row in ctxTableRows:
+    try:
+        save_skin(get_skin_information(row))
+    except ValueError as e:
+        print(str(e) + ": Could not be saved")
+    break
+
+# for link in skinLink:
+#     print(link.get_text())
+
         
     
 

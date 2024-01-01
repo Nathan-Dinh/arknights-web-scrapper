d7662afe9672ba46d7eef77fcd8e3f1a6958ff9e
nathandinh <nathan.dinh03@outlook.com>
2023-12-31 20:31:07 -0500

    Implemented operator skins

--- Operator-Information.py
+++ Operator-Information.py
@@ -1,13 +1,15 @@
 # Author: Nathan Dinh
-# Revision: December 29 2023 - Nathan Dinh 
+# Revision: December 31 2023 - Nathan Dinh 
 
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from dotenv import dotenv_values
 
+#region Configuration 
 # Imports all variables from .env file 
 config = {**dotenv_values("./config/.env.secret")}
+
 # Connect your application to your database
 client = MongoClient(config['MONGODB_CONNECTION_STRING'])
 database = client[config['DATABASE_NAME']]
@@ -17,30 +19,38 @@ skinCollection = database[config['SKINS_COLLECTION']]
 # Collection constraints
 operatorCollection.create_index('name', unique=True)
 skinCollection.create_index('skinName', unique=True)
+#endregion
 
-# Operate class
-class OperatorInformationTemplate:
-    def __init__(self,imageUrl,name,operatorTags,operatorArc,operatorProfile):
+#region Schema
+class OperatorInformationSchema:
+    def __init__(self,imageUrl,name,profession,position,attackType,operatorTags,operatorArc,operatorProfile):
         self.imageUrl = str(imageUrl)
-        self.name = str(name)
+        self.name = str(name).strip().lower()
+        self.profession = str(profession).strip().lower()
+        self.operatorArc = str(operatorArc).strip()
+        self.position = str(position).strip().lower()
+        self.attackType = str(attackType).strip().lower()
         self.operatorTags = operatorTags
-        self.operatorArc = str(operatorArc)
         self.operatorProfile = str(operatorProfile)
 
-class SkinInformationTemplate:
+class SkinInformationSchema:
     def __init__(self,imageUrl,skinName,operatorName):
         self.imageUrl = str(imageUrl)
-        self.skinName = str(skinName)
-        self.operatorName = str(operatorName)
+        self.skinName = str(skinName).strip().lower()
+        self.operatorName = str(operatorName).strip().lower()
+#endregion
 
-# Method will save the operators information to your database
+#region Methods
 def save_operator(operatorTemplate):
     try:
         postBody = {"imgUrl":operatorTemplate.imageUrl, 
                   "name": operatorTemplate.name,
-                  "recruitmentTag": operatorTemplate.operatorTags,
+                  "profession": operatorTemplate.profession,
                   "archetype": operatorTemplate.operatorArc,
-                  "profile": operatorTemplate.operatorProfile,}
+                  "position": operatorTemplate.position,
+                  "attackType": operatorTemplate.attackType,
+                  "recruitmentTag": operatorTemplate.operatorTags,
+                  "profile": operatorTemplate.operatorProfile}
         operatorCollection.insert_one(postBody)
         print(operatorTemplate.name + ":Saved")
     except Exception as e:
@@ -60,36 +70,54 @@ def save_skin(skinTemplate):
 
 def get_skin_information(htmlContext):
     try:
-        operatorName = htmlContext.find(class_="views-field views-field-field-skin-operator").a.get_text().lower()
-        skinLink = htmlContext.a['href']
-        req = requests.get(config['DOMAIN_NAME']+skinLink)
+        operatorName = htmlContext.find(class_="views-field views-field-field-skin-operator").a.get_text()
+        #Sends another request using the url retrieved from the html context
+        req = requests.get(config['DOMAIN_NAME'] + htmlContext.a['href'])
         ctx = BeautifulSoup(req.content, "html.parser")
+        
         imageLink = ctx.find(id="image-tab-1").a['href']
-        skinName = ctx.find(id="page-title").h1.get_text().lower()
-        return SkinInformationTemplate(imageLink,skinName,operatorName)
+        skinName = ctx.find(id="page-title").h1.get_text()
+        return SkinInformationSchema(imageLink,skinName,operatorName)
     except Exception as e:
-           print(e)
            raise ValueError(str(operatorName))
 
 def get_operator_information(htmlContext):
         try:
+            operatorName = htmlContext.find(id="page-title").h1.get_text()
             operatorImage = htmlContext.find(id="image-tab-1").a['href']
-            operatorName = htmlContext.find(id="page-title").h1.get_text().lower()
-            operatorTags = htmlContext.find_all(class_="tag-title")
-            operatorProfile = htmlContext.find_all(class_='profile-description')
-
+            #The operatorTags will return an array of tags
+            operatorTags = htmlContext.find_all(class_="tag-title") 
+            
+            #This will loop through what is returned in the operators tags  
             for t in range(len(operatorTags)) :
-                operatorTags[t] = operatorTags[t].get_text().strip()
+                operatorTags[t] = operatorTags[t].get_text()
+                if t == len(operatorTags) - 1 :
+                    operatorArc = operatorTags[t].strip()
+                    operatorTags.pop(len(operatorTags) - 1)
+                    break
+                operatorTags[t] = operatorTags[t].strip().lower()
+                    
+            operatorProfession = htmlContext.find(class_='profession-title').get_text()
+            operatorPosition = htmlContext.find(class_='position-cell').a.get_text()
+            operatorAttackType = htmlContext.find(class_="traits-cell").a.get_text()
+            operatorProfile = htmlContext.find_all(class_='profile-description')
+            operatorProfile = operatorProfile[0].get_text()
 
-            if t == len(operatorTags) - 1 :
-                operatorArc = operatorTags[t]
-                operatorTags.pop(len(operatorTags) - 1)
-
-            return  OperatorInformationTemplate(operatorImage,operatorName,operatorTags, operatorArc,operatorProfile[0].get_text())
+            return  OperatorInformationSchema(
+                operatorImage,
+                operatorName,
+                operatorProfession,
+                operatorPosition,
+                operatorAttackType,
+                operatorTags, 
+                operatorArc,
+                operatorProfile
+            )
         except Exception as e:
-           print(e)
            raise ValueError(str(operatorName))
+#endregion
 
+#region Operators
 reqOperatorListPage = requests.get(config['DOMAIN_NAME'] + config['OPERATOR_LIST_URL'])
 ctxOperatorListHtml = BeautifulSoup(reqOperatorListPage.content, "html.parser")
 operatorUrls = ctxOperatorListHtml.find_all(class_='operator-title-actual') # Collects the operators url 
@@ -101,8 +129,9 @@ for link in operatorUrls :
         save_operator(get_operator_information(operatorProfilePage))
     except ValueError as e:
         print(str(e) + ": Could not be saved")
-    break
+#endregion
 
+#region Skins
 reqSkinListPage = requests.get(config['DOMAIN_NAME'] + config['SKIN_LIST_URL'])
 ctxSkinListHtml = BeautifulSoup(reqSkinListPage.content, "html.parser")
 ctxSkinListHtml = ctxSkinListHtml.find(id="topic-511941")
@@ -114,4 +143,4 @@ for row in tableRows:
         save_skin(get_skin_information(row))
     except ValueError as e:
         print(str(e) + ": Could not be saved")
-    break
+#endregion
